2bc1bc04a0e6b44c72b6c7fe00ae43343d6887af
Lucas Eduardo <lucaseduardocampos457@gmail.com>
2024-10-17 20:27:52 -0300

    Limite de tela

--- gccgui.py
+++ gccgui.py
@@ -1,13 +1,18 @@
 from tkinter import *
 from tkinter import ttk
 
+
 class Application:
     def __init__(self, master=None):
-        #frm = frame
-        frm = ttk.Frame(root, padding=100)
-        frm.grid()
-        ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-        ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+        self.widget_master = Frame(master)
+        self.widget_master.pack()
+        self.msg = Label(self.widget_master, text="Primeiro Widget")
+        self.msg.pack()
 root = Tk()
+width_screen = (root.winfo_screenwidth())
+height_screen = (root.winfo_screenheight())
+root.geometry("700x400")
+root.maxsize(width=width_screen, height=height_screen)
+root.minsize(width=800, height=500)
 Application(root)
 root.mainloop()
