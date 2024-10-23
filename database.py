ff44aac5f45b4a16c18ef7eef4ed98e047c8d9f2
Lucas Eduardo <lucaseduardocampos457@gmail.com>
2024-10-23 02:24:31 -0300

    Banco de dados implementado

--- database.py
+++ database.py
@@ -1,1 +1,15 @@
 import psycopg2
+
+conexao = psycopg2.connect(
+database = 'postgres',
+host = 'amply-winged-bat.data-1.use1.tembo.io',
+user = 'postgres',
+password = 'ycxf7w5ecVuixXNx',
+port = '5432')
+
+print(conexao.info)
+print(conexao.status)
+
+cursor = conexao.cursor()
+cursor.execute('select * from teste')
+print(cursor.fetchall())
