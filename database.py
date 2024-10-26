af04a16c4e1289387c335500ef4dbf3a3cceeb04
Lucas Eduardo <lucaseduardocampos457@gmail.com>
2024-10-26 02:14:43 -0300

    Alterações

--- database.py
+++ database.py
@@ -2,9 +2,9 @@ import psycopg2
 
 conexao = psycopg2.connect(
 database = 'postgres',
-host = 'amply-winged-bat.data-1.use1.tembo.io',
+host = '',
 user = 'postgres',
-password = 'ycxf7w5ecVuixXNx',
+password = '',
 port = '5432')
 
 print(conexao.info)
